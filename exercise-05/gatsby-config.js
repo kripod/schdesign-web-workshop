39aa9f99f2183767d171eb6469bc61518f19a2f0
Kristóf Poduszló <kripod@protonmail.com>
2018-11-04 17:06:43 +0100

    Initialize an empty gatsby-starter-strict project for exercise-05

---
+++ exercise-05/gatsby-config.js
@@ -0,0 +1,7 @@
+module.exports = {
+  siteMetadata: {
+    title: 'Gatsby Strict Starter',
+    language: 'en',
+  },
+  plugins: ['gatsby-plugin-react-helmet', 'gatsby-plugin-typescript'],
+};
---
+++ exercise-05/src/pages/index.tsx
@@ -0,0 +1,10 @@
+import * as React from 'react';
+import Layout from '../components/Layout';
+
+const IndexPage = () => (
+  <Layout>
+    <p>Hello, world!</p>
+  </Layout>
+);
+
+export default IndexPage;
