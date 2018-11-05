5ec9833685707929f047eb68366dd5fd88bf35fd
Kristóf Poduszló <kripod@protonmail.com>
2018-11-05 11:19:26 +0100

    Add more annotation comments

--- exercise-05/src/components/Greeting.tsx
+++ exercise-05/src/components/Greeting.tsx
@@ -1,11 +1,24 @@
 import * as React from 'react';
 
 interface Props {
+  // Optional properties are denoted by a `?` at the end of the property name
+  // Specifying their value isn't mandatory, so they're ideal for props with a
+  // default value
+  // See: https://www.typescriptlang.org/docs/handbook/interfaces.html#optional-properties
   as?: React.ReactType;
   recipient: string;
 }
 
+// React DOM uses camelCase property naming convention, but user-defined
+// components must be capitalized, so the `as` prop must be aliased to `T`
+// See:
+// - https://reactjs.org/docs/jsx-in-depth.html#user-defined-components-must-be-capitalized
+// - https://reactjs.org/docs/glossary.html#jsx
+// - https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Operators/Destructuring_assignment#Assigning_to_new_variable_names
 const Greeting = ({ as: T = 'p', recipient }: Props) => (
+  // Think about curly braces as an escape hatch from the XML-like JSX syntax
+  // Any valid JS expression can be used inside braces, even one with JSX inside
+  // See: https://reactjs.org/docs/introducing-jsx.html#embedding-expressions-in-jsx
   <T>Hello, {recipient}!</T>
 );
 
--- exercise-05/src/pages/01-counter.tsx
+++ exercise-05/src/pages/01-counter.tsx
@@ -7,12 +7,25 @@ interface State {
   count: number;
 }
 
+// Use class components for modeling stateful logic
+// Re-renders are invoked by state (internal) or prop (external) changes
+// With React Hooks, function components may also be used for this purpose, see:
+// https://reactjs.org/docs/hooks-intro.html
 export default class CounterPage extends React.Component<Props, State> {
+  // Internal component state can be initialized by a class field declaration
   state = {
     count: 0,
   };
 
+  // Functions must be bound to the component instance to make sure they have
+  // access to component attributes like `this.props` and `this.state`
+  // See: https://reactjs.org/docs/faq-functions.html#how-do-i-bind-a-function-to-a-component-instance
   increaseCount = () => {
+    // The value and contents of `this.state` shall not be modified directly
+    // Instead, enqueue changes to the component state by calling a method which
+    // will eventually update `this.state` with the given values, optionally
+    // based on the previous `state` and `props`
+    // See: https://reactjs.org/docs/react-component.html#setstate
     this.setState(state => ({ count: state.count + 1 }));
   };
 
@@ -25,7 +38,12 @@ export default class CounterPage extends React.Component<Props, State> {
 
     return (
       <Layout>
-        <button type="button" onClick={this.decreaseCount}>
+        <button
+          type="button"
+          // Make sure not to call the function when passing it as an attribute
+          // See: https://reactjs.org/docs/faq-functions.html#why-is-my-function-being-called-every-time-the-component-renders
+          onClick={this.decreaseCount}
+        >
           -
         </button>
         {count}
