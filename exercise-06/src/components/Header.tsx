f415a5e6754bd627c5738edc98421df7b6f61331
Kristóf Poduszló <kripod@protonmail.com>
2018-11-12 09:42:12 +0100

    Make navbar sticky with a bottom margin

--- exercise-06/src/components/Header.tsx
+++ exercise-06/src/components/Header.tsx
@@ -33,46 +33,44 @@ export default class Header extends React.Component<Props, State> {
     const { isOpen } = this.state;
 
     return (
-      <div>
-        <Navbar color="light" light expand="md">
-          <StaticQuery
-            query={graphql`
-              {
-                site {
-                  siteMetadata {
-                    title
-                  }
+      <Navbar sticky="top" color="light" light expand="md" className="mb-2">
+        <StaticQuery
+          query={graphql`
+            {
+              site {
+                siteMetadata {
+                  title
                 }
               }
-            `}
-            render={data => (
-              <NavbarBrand href="/">{data.site.siteMetadata.title}</NavbarBrand>
-            )}
-          />
-          <NavbarToggler onClick={this.toggle} />
+            }
+          `}
+          render={data => (
+            <NavbarBrand href="/">{data.site.siteMetadata.title}</NavbarBrand>
+          )}
+        />
+        <NavbarToggler onClick={this.toggle} />
 
-          <Collapse isOpen={isOpen} navbar>
-            <Nav className="mr-auto" navbar>
-              <NavItem>
-                <GatsbyNavLink to="/">Kezdőlap</GatsbyNavLink>
-              </NavItem>
-              <NavItem>
-                <GatsbyNavLink to="/technology/">Technológia</GatsbyNavLink>
-              </NavItem>
-              <NavItem>
-                <GatsbyNavLink to="/design/">Design</GatsbyNavLink>
-              </NavItem>
-              <NavItem>
-                <GatsbyNavLink to="/economy/">Gazdaság</GatsbyNavLink>
-              </NavItem>
-            </Nav>
+        <Collapse isOpen={isOpen} navbar>
+          <Nav className="mr-auto" navbar>
+            <NavItem>
+              <GatsbyNavLink to="/">Kezdőlap</GatsbyNavLink>
+            </NavItem>
+            <NavItem>
+              <GatsbyNavLink to="/technology/">Technológia</GatsbyNavLink>
+            </NavItem>
+            <NavItem>
+              <GatsbyNavLink to="/design/">Design</GatsbyNavLink>
+            </NavItem>
+            <NavItem>
+              <GatsbyNavLink to="/economy/">Gazdaság</GatsbyNavLink>
+            </NavItem>
+          </Nav>
 
-            <Form inline className="my-2 my-lg-0">
-              <Input type="search" placeholder="Keresés…" />
-            </Form>
-          </Collapse>
-        </Navbar>
-      </div>
+          <Form inline className="my-2 my-lg-0">
+            <Input type="search" placeholder="Keresés…" />
+          </Form>
+        </Collapse>
+      </Navbar>
     );
   }
 }
